d52cb946fcb680ac5976e041ec896e014bf68516
Fernando Izquierdo Romera <fernando.izquierdo@nucleoo.com>
2022-06-23 18:31:51 +0200

    Comments on index

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = new Koa();
 var port = process.env.PORT;
 var server = null;
 
+// Mongoose connection to database
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useFindAndModify: true,
@@ -21,13 +22,17 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => {console.log("DB connected");
 
 })
-.catch((err)=> console.error("DB Connection errror", err));
+.catch((err)=> console.error("DB connection errror", err));
 
+// Plugins
 app.use(bodyParser());
 app.use(json());
+// Routes of router
 app.use(routes.routes());
+// Only respond to routes
 app.use(routes.allowedMethods());
 
+// Launch server
 server = app.listen(port, err => {
     if (err) throw err;
     console.log(`> Running on localhost:${port}`);
