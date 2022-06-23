86ce1abf9dec4afea3d79ae579026c577f8b3aea
Fernando Izquierdo Romera <fernando.izquierdo@nucleoo.com>
2022-06-23 18:28:04 +0200

    Mongoose connection #1 #2

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ const json = require('koa-json');
 const bodyParser = require('koa-bodyparser');
 const routes = require('./routes/routes.js');
 const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = new Koa();
 var port = process.env.PORT;
 var server = null;
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    autoCreate : true,
+    setDefaultsOnInsert: true
+})
+.then(() => {console.log("DB connected");
+
+})
+.catch((err)=> console.error("DB Connection errror", err));
 
 app.use(bodyParser());
 app.use(json());
